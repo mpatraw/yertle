d7232176ae87870423861643114fded2f522d335
Michael Patraw <michaelpatraw@gmail.com>
2016-11-18 00:13:56 -0800

    add pythagoras tree example.

---
+++ examples/pythagoras_tree.rs
@@ -0,0 +1,29 @@
+extern crate yertle;
+
+use yertle::*;
+use yertle::TurtleCommand::*;
+
+fn main() {
+    let mut t = Turtle::new("yertle: pythagoras", 800, 600).unwrap();
+
+    let mut l = LSystem::new();
+    l.variable('0', "1[0]0");
+    l.variable('1', "11");
+    l.constant('[');
+    l.constant(']');
+    let mut m = Machine::new();
+    m.binds('0', vec![Color(0, 255, 0), Forward(2.0), Color(255, 255, 255)]);
+    m.bind('1', Forward(2.0));
+    m.binds('[', vec![PushState, Left(45.0)]);
+    m.binds(']', vec![PopState, Right(45.0)]);
+    let src = l.grow_n("0", 8);
+    println!("#cmds: {}", src.len());
+    let mut prog = m.compile(src.as_str());
+
+    t.goto(400.0, 600.0);
+    t.face(-90.0);
+    while t.update(0.05) {
+        prog.step(&mut t, 50);
+    }
+}
+
